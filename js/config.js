d9c0dbf29654f0a7c1e92d34d5e0f89c076d5210
GiftMawashaML <giftamazon1055@gmail.com>
2025-04-22 19:02:35 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-1_K4xGoBzio', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4anvtmdusf8po83aoe71q28ahk', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-west-1_K4xGoBzio', // 
+        userPoolClientId: '4anvtmdusf8po83aoe71q28ahk', //
         clientSecret: '15e015gph6kup3fpn83iholnoj7seeu94cefsbm41bh9o8hoo6tg', //
-        region: 'us-west-1' // e.g. us-east-2
+        region: 'us-west-1' //
     },
     api: {
-        invokeUrl: '' // Optional API Gateway endpoint
+        invokeUrl: '' //
     }
 };
